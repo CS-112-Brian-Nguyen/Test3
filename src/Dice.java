7123290d57d9e06932106fc5e59bc3f9d456eb68
Brian Nguyen <brianthenguyens@gmail.com>
2015-11-19 23:50:57 -0800

    11/19 Reupload

---
+++ src/Dice.java
@@ -0,0 +1,124 @@
+// Dice class
+
+import java.util.Scanner;
+
+public class Dice 
+{
+	private int diceValue;
+	private String userGuess;
+	private String userGuessToInt;
+	private String answer;
+	private int numOfDices;
+	private int userPoints = 0;
+	private int highValue = 0;
+	private int userGuessInt = 0;
+	private int totalGames = 0;
+	
+	Scanner input = new Scanner(System.in);
+	
+	// Constructor
+	public Dice()
+	{
+		this.numOfDices = 1;
+	}
+	
+	public int getNumberOfDice()
+	{
+		// Get number of dice from player
+		System.out.print("How many dices do you want to play with? ");
+		numOfDices = input.nextInt();
+		input.nextLine();
+		return numOfDices;
+	}
+	
+	public String getUserGuess()
+	{
+		// Get user guess
+		System.out.printf("Take a guess: 'low' or 'high'? \n"
+				+ "low = 1 to %d\n"
+				+ "high = %d to %d"
+				+ "\nEnter guess: ", ((highValue / 2) - 1), ((highValue / 2) + 1), highValue);
+		userGuess = input.nextLine();
+		return userGuess;
+	}
+	
+	public int Throw(int pNumOfDices)
+	{
+		// total game counter
+		totalGames++;
+		
+		// get dice value
+		diceValue = (int) (Math.random() * (pNumOfDices * 6)) + 1;
+		
+		return diceValue;
+	}
+	
+	public int highValue(int pNumOfDices)
+	{
+		highValue = (pNumOfDices * 6);
+		return highValue;
+	}
+	
+	public static boolean isNumeric(String userGuess)
+	{
+	  return userGuess.matches("-?\\d+(\\.\\d+)?");
+	}
+	
+	public void userGuessInt(String userGuess)
+	{
+		// This will convert from string to int to give us an integer value
+		
+		if(userGuess.matches("-?\\d+(\\.\\d+)?"))
+		{
+			userGuessInt = Integer.parseInt(userGuess);
+		}
+	}
+	
+	public void displayRound()
+	{
+		
+		if ((userGuess.equalsIgnoreCase("low")) && (diceValue < (highValue / 2)))
+		{
+			System.out.print("You Win! The dice rolled " + diceValue + " which is a low from a " + highValue + " sided dice.");
+			userPoints++;
+		}
+		else if ((userGuess.equalsIgnoreCase("low")) && (diceValue > (highValue / 2)))
+		{
+			System.out.print("You lose! The dice rolled " + diceValue + " which is a high from a " + highValue + " sided dice.");
+			userPoints--;
+		}
+		else if ((userGuess.equalsIgnoreCase("high")) && (diceValue > (highValue / 2)))
+		{
+			System.out.print("You Win! The dice rolled " + diceValue + " which is a high from a " + highValue + " sided dice.");
+			userPoints++;
+		}
+		
+		else if ((userGuess.equalsIgnoreCase("high")) && (diceValue < (highValue / 2)))
+		{
+			System.out.print("You lose! The dice rolled " + diceValue + " which is a low from a " + highValue + " sided dice.");
+			userPoints--;
+		}
+		
+		else if (diceValue == (highValue / 2) && (userGuess.equalsIgnoreCase("high") || (userGuess.equalsIgnoreCase("low"))))
+		{
+			System.out.print("You lose! The dice rolled " + diceValue + " which is not the range of a " + highValue + " sided dice. So neigther high nor low.");
+			userPoints--;
+		}
+	}
+	
+	
+	public String playAgain()
+	{
+		System.out.print("\n\nDo you want to play again? (Yes or No): ");
+		answer = input.next();
+		
+		return answer;
+	}
+	
+	public void finalResult()
+	{
+		System.out.println("\nThese are the final results:");
+		System.out.println("The total number of games played were: " + totalGames);
+		System.out.println("Total user points: " + userPoints);
+	}
+}
---
+++ src/HiLowGame.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class HiLowGame 
+{
+
+	public static void main(String[] args) 
+	{
+		// Variables
+		String userGuess;
+		String answer = "NO";
+		int numOfDice;
+		int randomVal = 0;	
+		int highVal = 0;
+		int userGuessInt = 0;
+		
+		Scanner input = new Scanner(System.in);
+		
+		// Create new object of Dice type
+		Dice hiLow = new Dice();
+		
+		do
+		{
+			// get number of dice from user
+			numOfDice = hiLow.getNumberOfDice();
+			
+			// get random value from computer
+			randomVal = hiLow.Throw(numOfDice);
+			
+			// debug Show dice number.
+			System.out.println(randomVal);
+			
+			// Generate high value
+			highVal = hiLow.highValue(numOfDice);
+			
+			// get user guess
+			userGuess = hiLow.getUserGuess();
+			
+			// convert String to Int if needed
+			hiLow.userGuessInt(userGuess);
+			
+			// display this round result
+			hiLow.displayRound();
+			
+			// Ask user to play again
+			answer = hiLow.playAgain();
+			
+		}while(answer.equalsIgnoreCase("YES"));
+		
+		
+		// display final tally
+		hiLow.finalResult();
+		
+	}
+
+}
